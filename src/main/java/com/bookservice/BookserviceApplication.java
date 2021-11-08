669c30df718ef0e6ca8e4437336b9979a583c95c
zunggzing <hello.trungvinh@gmail.com>
2021-11-08 19:43:39 +0700

    Update config: pom, application.yml

--- src/main/java/com/bookservice/BookserviceApplication.java
+++ src/main/java/com/bookservice/BookserviceApplication.java
@@ -2,8 +2,10 @@ package com.bookservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BookserviceApplication {
 
 	public static void main(String[] args) {
