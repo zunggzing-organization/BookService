865d45f3072437694773b8693dc9473ddcba6438
zunggzing <hello.trungvinh@gmail.com>
2021-11-08 17:11:31 +0700

    Update project: application.yml, add lib, run in test

--- src/main/java/com/bookservice/service/BookImpl.java
+++ src/main/java/com/bookservice/service/BookImpl.java
@@ -2,6 +2,8 @@ package com.bookservice.service;
 
 import com.bookservice.entity.Book;
 import com.bookservice.repository.BookRepository;
+import io.github.resilience4j.ratelimiter.RequestNotPermitted;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +13,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 @Service
 @Slf4j
-
 public class BookImpl implements BookService{
     @Autowired
     private BookRepository bookRepository;
@@ -33,7 +34,7 @@ public class BookImpl implements BookService{
 
     @Override
 
-    @Retry(name = "basic")
+    @RateLimiter(name = "basic", fallbackMethod = "fallMethodRateLimiter")
 
     public Book getBook(Long id) {
         log.info(LocalDateTime.now() + "IN GET FACULTY BY ID");
@@ -44,5 +45,9 @@ public class BookImpl implements BookService{
     public void deleteBook(Long id) {
         bookRepository.deleteById(id);
 
+    }
+    public Book fallMethodRateLimiter(int id, RequestNotPermitted rnp) {
+        log.info("Request Not Permitted By Many Request");
+        return null;
     }
 }
